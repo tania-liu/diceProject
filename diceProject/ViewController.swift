f0de90149d1013c48fe229ffff72fd2f80a541f6
Tania Liu <tanialiu430006@gmail.com>
2024-02-27 21:03:15 +0800

    Initial Commit

---
+++ diceProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diceProject
+//
+//  Created by 劉俐廷 on 2024/2/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
